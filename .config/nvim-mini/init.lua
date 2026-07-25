5af7d1a056fd31d4fe3d75ca360eb8c74dd8e604
Roman Dordzheev <roman.te9c@gmail.com>
2026-07-25 12:40:46 +0300

    up

--- .config/nvim-mini/init.lua
+++ .config/nvim-mini/init.lua
@@ -18,14 +18,20 @@ vim.opt.showmode = false
 vim.keymap.set('n', 'o', 'o<esc>')
 vim.keymap.set('n', 'O', 'O<esc>')
 
+vim.opt.keymap = "russian-programmers-dvorak"
+vim.opt.iminsert = 0
+
 local function toggle_keymap()
-    if vim.opt.keymap:get() == "" then
-        vim.opt.keymap = "russian-programmers-dvorak"
-        print('ru')
-    else
-        vim.opt.keymap = ""
+    if vim.opt.iminsert:get() == 1 then
+        vim.opt.iminsert = 0
         print('en')
+    else
+        vim.opt.iminsert = 1
+        print('ru')
+    end
+
+    if vim.api.nvim_get_mode().mode == 'i' then
+        vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes('<C-^>', true, false, true), 'n', false)
     end
 end
-
 vim.keymap.set({'n', 'i'}, '<C-l>', toggle_keymap, { desc = "toggle between russian and english keymaps" })
